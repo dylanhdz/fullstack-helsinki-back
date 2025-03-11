afd8be81395142db3f0b0f051ddb4465889e751f
Dylan Hernandez <105333109+dylanhdz@users.noreply.github.com>
2025-03-11 18:09:32 -0500

    3.1 to 3.6

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json())
+
+app.get('/info', (request, response) => {
+    const welcome = 'Phonebook has info for ' + persons.length + ' people'
+    const date = Date(request.headers.date)
+    response.send('<p>' + welcome + '</p>' + '<p>' + date + '</p>')
+  
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const generateId = () => {
+  const randomId = Math.floor(Math.random() * 1000)
+  return randomId.toString()
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'either name or number is missing' 
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+  if (person) {
+    response.json(person)
+  } else {
+    console.log('Not Found')
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
