d15b51f36887a35afca47503fd2124b3efa01951
Dylan Hernandez <105333109+dylanhdz@users.noreply.github.com>
2025-03-14 17:34:19 -0500

    Exercises 3-19 to 3-21

--- models/person.js
+++ models/person.js
@@ -14,16 +14,32 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
+function validateNumber(number) {
+  const parts = number.split('-')
+  if (parts.length != 2) {
+    return false
+  }
+  if (parts[0].length < 2 || parts[0].length > 3) {
+    return false
+  }
+  return true
+}
+const customValidator = [
+  validateNumber,
+  'Number has to have two parts separated by -. The first part has to be 2-3 characters long.'
+]
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 3
+    minlength: [3, 'Name must be at least 3 characters long.']
   },
   number: {
     type: String,
     required: true,
-    minlength: 8
+    minlength: [8, 'Number must be at least 8 characters long'],
+    validate: customValidator
   }
   /*
     Previously.
