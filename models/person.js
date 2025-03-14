227e83eaed0460905502005909b98dc9bc9284f7
Dylan Hernandez <105333109+dylanhdz@users.noreply.github.com>
2025-03-14 16:13:42 -0500

    Validations

--- models/person.js
+++ models/person.js
@@ -15,8 +15,21 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8
+  }
+  /*
+    Previously.
+    name: String,
+    number: String,
+  */
 })
 
 personSchema.set('toJSON', {
